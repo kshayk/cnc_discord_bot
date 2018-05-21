43a69656f2ec3cf47d5fdeaa246e12f58f159085
Unknown <kshayk0@gmail.com>
2018-05-21 23:03:40 +0300

    Added support for $report poll

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const botconfig = require('./botconfig.json');
 const Discord = require('discord.js');
 const mongojs = require('mongojs');
-const db = mongojs('cnc_bot', ['afks']); //specifiyng the database and table(s)
+const db = mongojs('cnc_bot', ['afks', 'reports']); //specifiyng the database and table(s)
+
 const api_keys = require('./api_keys.js');
 const request = require('request');
 const language_map = require('./language_index.js');
@@ -20,7 +21,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
     console.log(`bot prefix: ${botconfig.prefix}`);
     console.log(`${bot.user.username} is online`);
-    bot.user.setActivity("$ to activate");
+    bot.user.setActivity("$ to activate", {type: "PLAYING"}); //type can also be "WATCHING" for stream watch and such. its not mandatory to declare type, it will be automatically "PLAYING"
 });
 
 bot.on("message", async message => {
@@ -32,12 +33,7 @@ bot.on("message", async message => {
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
-    //!say hello (gets only the hello with those splits)
-    if(cmd === `${prefix}hello`) {
-        return message.channel.send("hello!");
-    }
-
-    //get current weather for a specific location
+    //get current weather for a specific location: $weather israel
     if(cmd === `${prefix}weather`) {
         var location_string = '';
 
@@ -92,6 +88,7 @@ bot.on("message", async message => {
         });
     }
 
+    //translate a text: $translate hebew hello world
     if(cmd === `${prefix}translate`) {
         if(typeof args[0] === 'undefined') {
             //return list of options
@@ -167,16 +164,6 @@ bot.on("message", async message => {
         });
     }
 
-    if(cmd === `${prefix}help`) {
-        var botembed = new Discord.RichEmbed()
-            .setDescription('CNC Bot Commands')
-            .setColor('#81a9e8')
-            .addField('$weather', 'Shows weather of a current location. Usage: $weather london uk')
-            .addField('$translate', 'Translates a certain word/phrase according to give language and text. Usage: $translate dutch hey how are you doing');
-
-        return message.channel.send(botembed);
-    }
-
     if(cmd === `${prefix}afk`) {
         // console.log(message);
         var server_id = message.member.guild.id;
@@ -208,6 +195,139 @@ bot.on("message", async message => {
                 });
             }
         });
+    }
+
+    if(cmd === `${prefix}report`) {
+        let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0])); // this will find the user that was mentioned in the message
+
+        if( ! rUser) {
+            return message.channel.send(embedErrorMessage('Could not find the user'));
+        }
+
+        let reason = args.join(" ").slice(22);
+
+        if(reason.replace(" ", "") === null || reason.replace(" ", "") === "") {
+            return message.channel.send(embedErrorMessage("You must provide a reason for the report"));
+        }
+
+        let reportsChannel = message.guild.channels.find(`name`, 'reports') || message.guild.channels.find(`name`, 'spam');
+
+        if( ! reportsChannel) {
+            return message.channel.send(embedErrorMessage("To report a user, the server must have a 'reports' channel"));
+        }
+
+        var reportConfirmEmbed = new Discord.RichEmbed()
+            .setDescription("Report user?")
+            .setColor("#15f153")
+            .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+            .addField("Reason", reason)
+            .setFooter("at least 4 votes (positive or negative) required to proceed with the reporting. After a minute this poll will close.")
+
+        var embedConfirm = message.channel.send(reportConfirmEmbed).then(async function (my_message) {
+            await my_message.react('👍');
+            await my_message.react('👎');
+
+            // Create a reaction collector
+            var filter = (reaction, user) => reaction.emoji.name === '👍' || reaction.emoji.name === '👎'
+            var collected = await my_message.awaitReactions(filter,  { time: 10000 });
+            var agree = await collected.get('👍') && await collected.get('👍').count-1;
+            var disagree = await collected.get('👎') && await collected.get('👎').count-1;
+
+            if(agree && agree >= 3 && agree > disagree) {
+                my_message.delete();
+
+                //check if strikes are more than 3, if so send message asking for kick.
+                db.reports.find({server_id: message.member.guild.id , user_id: rUser.id}, (err, docs) => {
+                    if(err || docs.length === 0) {
+                        console.log('empty');
+                        // update a record in the collection
+                        db.reports.insert(
+                            // find record with name "MyServer"
+                            { user_id: rUser.id, server_id: message.member.guild.id, strikes: 1},
+                            (err2, docs2) => {
+                                let reportEmbed = new Discord.RichEmbed()
+                                    .setDescription("Reports")
+                                    .setColor("#15f153")
+                                    .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+                                    .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+                                    .addField("Channel", message.channel)
+                                    .addField("Time", message.createdAt)
+                                    .addField("Strikes", "1")
+                                    .addField("Reason", reason);
+
+                                return reportsChannel.send(reportEmbed);
+                            }
+                        );
+                    } else {
+                        var new_strike = docs[0].strikes + 1;
+
+                        db.reports.update(
+                            // find record with that ID
+                            {_id: docs[0]._id},
+                            //use the $inc function to add 1 to the strikes
+                            {
+                                server_id: message.member.guild.id,
+                                user_id: rUser.id,
+                                strikes: new_strike
+                            },
+                            //
+                            (err2, docs2) => {
+                                let reportEmbed = new Discord.RichEmbed()
+                                    .setDescription("Reports")
+                                    .setColor("#15f153")
+                                    .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+                                    .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+                                    .addField("Channel", message.channel)
+                                    .addField("Time", message.createdAt)
+                                    .addField("Strikes", `${new_strike}`)
+                                    .addField("Reason", reason);
+
+                                reportsChannel.send(reportEmbed);
+
+                                if(new_strike >= 5) {
+                                    let kickEmbed = new Discord.RichEmbed()
+                                        .setDescription("Kick user?")
+                                        .setColor("#efad5d")
+                                        .addField("Violator", `The user ${rUser} with ID: ${rUser.id} got ${new_strike} reports on his account. To kick the accound do: ${prefix}kick {account}`);
+
+                                    return reportsChannel.send(kickEmbed);
+                                }
+
+                                return;
+                            }
+                        )
+                    }
+                });
+
+                message.channel.send(`Successfully reported user ${rUser}`);
+            } else {
+                my_message.delete();
+                message.channel.send(`Agree/disagree vote ratio is not sufficient to report the user ${rUser}`);
+            }
+        });
+        // message.delete().catch(O_o=>{}); // deletes the last message, currently not working need to understand how to do that
+    }
+
+    if(cmd === `${prefix}testemoji`) {
+        message.react('✔');
+        // Create a reaction collector
+        const filter = (reaction, user) => reaction.emoji.name === '✔'  && user.id === message.author.id //only checks for this emoji
+        const collector = message.createReactionCollector(filter, { time: 15000 });
+        collector.on('collect', r => console.log(`Collected ${r.emoji.name}`)); //when a new emoji that passes the filter is getting reacted for that message
+        collector.on('end', collected => console.log(`Collected ${collected.size} items`)); //after the timeout is finished getting the collected overall emojis
+    }
+
+    if(cmd === `${prefix}help`) {
+        var botembed = new Discord.RichEmbed()
+            .setDescription('CNC Bot Commands')
+            .setColor('#81a9e8')
+            .addField('$weather', 'Shows weather of a current location. Usage: $weather london uk')
+            .addField('$translate', 'Translates a certain word/phrase according to given language and text. Usage: $translate dutch hey how are you doing')
+            .addField('$afk', 'Toggle your AFK status')
+            .addField('$report', 'Creates a poll to report a user. Must have a "reports" text channel to support this method. Usage: $report @user#123 reason')
+
+        message.author.sendMessage(botembed);
+        return message.channel.send("Sent bot usage instruction via private message.");
     }
 
     if(cmd === `DC`) {
